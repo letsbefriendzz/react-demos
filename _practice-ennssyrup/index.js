c4a4aa9d031d6fd13227cc8cb1cae06558b75e65
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-03-21 19:55:57 -0400

    ennssyrup changes

---
+++ _practice-ennssyrup/index.js
@@ -0,0 +1,9 @@
+const buttonPress = () => {
+    const titleElement = document.getElementById('pl1');
+    titleElement.scrollIntoView({behavior: 'smooth'});
+}
+
+const toBottomSection = () => {
+    const bottomSec = document.getElementById('pl2');
+    bottomSec.scrollIntoView({behavior: 'smooth'});
+}
